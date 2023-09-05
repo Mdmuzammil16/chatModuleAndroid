5db6f47668a5275ad229350011a372803e6d63e1
mohammedmuzammil <fazil8785@gmail.com>
2023-09-05 17:39:57 +0530

    puslishing added

--- app/src/main/java/com/andro/chats/MainActivity.kt
+++ app/src/main/java/com/andro/chats/MainActivity.kt
@@ -2,12 +2,14 @@ package com.andro.chats
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dosolutions.mymod1.MyToaster
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        MyToaster().DisplayToast(applicationContext, "this is message")
 
     }
 }
